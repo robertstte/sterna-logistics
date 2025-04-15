520835dcad3ea775656d61046523f46818fdfe17
robertstte <robertstefanvasile29@gmail.com>
2025-04-15 21:23:47 +0200

    Se crean traducciones

---
+++ lang/en/translations.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    "header" => [
+        "home" => "Home",
+        "about" => "About Us",
+        "contact" => "Contact",
+        "spanish" => "Spanish",
+        "english" => "English",
+        "language" => "Language",
+        "access" => "Access"
+    ], 
+    "landing" => [
+        "hero" => [
+            "title" => "Less weight for you, more efficiency for your logistics",
+            "cta" => "Try out"
+        ], 
+        "localization" => [
+            "title" => "Locate my cargo",
+            "subtitle" => "Enter the order number we have associated with it.",
+            "search" => "Search",
+            "help" => "I'm having trouble finding my order."
+        ],
+        "features" => [
+            "title" => "Everything you need at one place",
+            "security" => "Security",
+            "robustness" => "Robustness",
+            "speed" => "Speed",
+            "control" => [
+                "title" => "Full control",
+                "message" => "Monitor and manage your orders in real time, all from a single platform."
+            ],
+            "transport" => [
+                "title" => "Multimodal transport",
+                "message" => "Manage shipments through various forms of transport."
+            ],
+            "notified" => [
+                "title" => "Always notified",
+                "message" => "Receive updates at instantly on every movement of your order."
+            ],
+            "invoicing" => [
+                "title" => "Easy invoicing",
+                "message" => "All up to date and without complications, ready for when you need it."
+            ]
+        ]
+    ],
+    "footer" => [
+        "copyright" => "© 2025 Sterna. All rights reserved.",
+        "privacy" => "Privacy Policy and Terms of Use."
+    ]
+];
---
+++ lang/es/translations.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    "header" => [
+        "home" => "Inicio",
+        "about" => "Nosotros",
+        "contact" => "Contacto",
+        "spanish" => "Español",
+        "english" => "Inglés",
+        "language" => "Idioma",
+        "access" => "Acceso"
+    ],
+    "landing" => [
+        "hero" => [
+            "title" => "Menos peso para ti, más eficiencia para tu logística",
+            "cta" => "Probar"
+        ],
+        "localization" => [
+            "title" => "Localizar mi carga",
+            "subtitle" => "Introduzca el número de orden que le hemos asociado.",
+            "search" => "Buscar",
+            "help" => "Tengo problemas para encontrar mi orden."
+        ],
+        "features" => [
+            "title" => "Todo lo que necesitas en un único lugar",
+            "security" => "Seguridad",
+            "robustness" => "Robustez",
+            "speed" => "Rapidez",
+            "control" => [
+                "title" => "Control total",
+                "message" => "Monitorea y gestiona tus pedidos en tiempo real, todo desde una sola plataforma."
+            ],
+            "transport" => [
+                "title" => "Transporte multimodal",
+                "message" => "Gestiona envíos mediante diversas formas de transporte."
+            ],
+            "notified" => [
+                "title" => "Siempre notificado",
+                "message" => "Recibe actualizaciones al instante sobre cada movimiento de tu orden."
+            ],
+            "invoicing" => [
+                "title" => "Fácil facturación",
+                "message" => "Todo al día y sin complicaciones, listo para cuando lo necesites."
+            ]
+        ]
+     ],
+    "footer" => [
+        "copyright" => "© 2025 Sterna. Todos los derechos reservados.",
+        "privacy" => "Política de privacidad y Términos de uso."
+    ]
+];
