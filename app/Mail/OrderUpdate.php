1adbe27916445ad93339de05fde948c71dd821ce
robertstte <robertstefanvasile29@gmail.com>
2025-05-25 14:46:56 +0200

    Se crea mail para la acutalización de estado

---
+++ app/Mail/OrderUpdate.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class OrderUpdate extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $date;
+    public $name;
+    public $status;
+    public $description;
+    public $arrival_date;
+    public $observations;
+    public $order;
+
+    public function __construct($date, $name, $status, $description, $arrival_date, $observations, $order)
+    {
+        $this->date = $date;
+        $this->name = $name;
+        $this->status = $status;
+        $this->description = $description;
+        $this->arrival_date = $arrival_date;
+        $this->observations = $observations;
+        $this->order = $order;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Update: Your order status has changed',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.orderUpdate',
+            with: [
+                'date' => $this->date,
+                'name' => $this->name,
+                'status' => $this->status,
+                'description' => $this->description,
+                'arrival_date' => $this->arrival_date,
+                'observations' => $this->observations,
+                'order' => $this->order
+            ]
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/orderUpdate.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Password Change Confirmation</title> 
+    </head>
+    <body>
+        <header>
+            <p class="logo">Sterna.</p>
+        </header>
+        <main>
+            <p class="name">Estimado/a {{ $name }}</p>
+            <div class="content">
+                <p>Queremos informale que su orden <strong>Nº {{ $order }}</strong> ha cambiado de estado el <strong>{{ $date }}.</strong></p>
+                <p>Descripción: <strong>{{ $description }}</strong></p>
+                <p>Estado: <strong>{{ $status }}</strong></p>
+                <p>Fecha llagada prevista: <strong>{{ $arrival_date }}</strong></p>
+                <p>Observaciones: <strong>{{ $observaciones }}</strong></p>
+                <p>Si tienes alguna pregunta o necesitas ayuda, contáctanos en <strong>soporte@sterna.es</strong></p>
+                <p>Atentamente, el quipo de Sterna.</p>
+            </div>
+        </main>
+        <footer>
+            <p class="copyright">&copy {{ date('Y') }} Sterna. Todos los derechos reservados.</p>
+            <p class="privacy">Política de privacidad y Términos de uso.</p>
+        </footer>
+    </body>
+</html>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap');
+    header {
+        height: 100px;
+        background-color: rgb(0, 31, 63);
+    }
+    main {
+        margin-bottom: 50px;
+    }
+    .logo {
+        font-size: 64px;
+        padding-top: 10px;
+        font-weight: 700;
+        margin-left: 25px;
+        font-family: "Onest";
+        color: rgb(255, 255, 255);
+    }
+    .name {
+        font-size: 24px;
+        margin-top: 50px;
+        font-weight: 600;
+        margin-left: 25px;
+        margin-right: 25px;
+        font-family: "Onest";
+    }
+    .content {
+        font-size: 16px;
+        margin-top: 15px;
+        margin-left: 25px;
+        margin-right: 25px;
+        margin-bottom: 15px;
+        font-family: "Onest";
+    }
+    strong {
+        color: rgb(0, 168, 255);
+    }
+    footer {
+        bottom: 0;
+        width: 100%;
+        height: 100px;
+        display: flex;
+        font-size: 14px;
+        font-weight: 600;
+        font-family: "Onest";
+        color: rgb(255, 255, 255);
+        background-color: rgb(0, 31, 63);
+    }
+    .copyright {
+        text-align: left;
+        margin-left: 25px;
+        margin-top: 37.5px;
+    }
+
+    .social {
+        gap: 30px;
+        padding: 0;
+        display: flex;
+        list-style: none;
+        margin-top: 30px;
+    }
+
+    .privacy {
+        text-align: right;
+        margin-left: auto;
+        margin-top: 37.5px;
+        margin-right: 25px;
+    }
+</style>
