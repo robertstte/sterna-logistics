3a5b798942df8046512380534d19a23aa62bc13f
robertstte <robertstefanvasile29@gmail.com>
2025-05-26 18:45:21 +0200

    Migración de notificaciones

--- database/migrations/2025_04_20_100457_users.php
+++ database/migrations/2025_04_20_100457_users.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->string('google_id')->unique()->nullable();
             $table->string('avatar')->nullable();
+            $table->boolean('notifications')->default(true);
             $table->timestamps();
         });
     }
